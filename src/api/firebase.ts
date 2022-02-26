d646b7cdd069171cf14debe0af285b27f7933ff2
Arturov Kirill <kaarturov@sberbank.ru>
2022-02-26 10:08:20 +0300

    firebase reconnect

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,14 +2,24 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyCUwIN5IBgCq8bJBIEPYGRwcMA-7uYW86Q",
-  authDomain: "launch-grid.firebaseapp.com",
-  projectId: "launch-grid",
-  storageBucket: "launch-grid.appspot.com",
-  messagingSenderId: "835802745353",
-  appId: "1:835802745353:web:e7e9ef3d9303fbc8c386d8",
-  measurementId: "G-HSX4PVPN35",
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyCUwIN5IBgCq8bJBIEPYGRwcMA-7uYW86Q",
+//   authDomain: "launch-grid.firebaseapp.com",
+//   projectId: "launch-grid",
+//   storageBucket: "launch-grid.appspot.com",
+//   messagingSenderId: "835802745353",
+//   appId: "1:835802745353:web:e7e9ef3d9303fbc8c386d8",
+//   measurementId: "G-HSX4PVPN35",
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDO6QJz2VYi_WfmdGXVTERx0lZZqxh7xig",
+  authDomain: "launchtab-81b06.firebaseapp.com",
+  projectId: "launchtab-81b06",
+  storageBucket: "launchtab-81b06.appspot.com",
+  messagingSenderId: "862212900546",
+  appId: "1:862212900546:web:d804f07dbbd3381ca4dd5b",
+  measurementId: "G-2WJV9X5747",
 };
 
 export const app = firebase.initializeApp(firebaseConfig);

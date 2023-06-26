f918c4010ebbd154dbcdea9bb3675c16ecad9692
Kirill <curillaenator@gmail.com>
2023-06-26 15:27:04 +0700

    cfg

--- kit/drawer/src/styles/styles.ts
+++ kit/drawer/src/styles/styles.ts
@@ -3,10 +3,10 @@ import styles from './styles.module.scss';
 type TransitionProps = 'enter' | 'enterFrom' | 'enterTo' | 'leave' | 'leaveFrom' | 'leaveTo';
 
 export const overlayTransitions: Record<TransitionProps, string> = {
-  enter: styles.overlay_enter,
-  enterFrom: styles.overlay_enter_from,
-  enterTo: styles.overlay_enter_to,
-  leave: styles.overlay_leave,
-  leaveFrom: styles.overlay_leave_from,
-  leaveTo: styles.overlay_leave_to,
+  enter: styles['overlay_enter'],
+  enterFrom: styles['overlay_enter_from'],
+  enterTo: styles['overlay_enter_to'],
+  leave: styles['overlay_leave'],
+  leaveFrom: styles['overlay_leave_from'],
+  leaveTo: styles['overlay_leave_to'],
 };

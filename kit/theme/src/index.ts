34fae37df8ff3cf776bed129f67a6ecfd8b90975
Kirill <curillaenator@gmail.com>
2023-06-27 21:23:54 +0700

    theme pkg

---
+++ kit/theme/src/index.ts
@@ -0,0 +1,11 @@
+export { hexa } from './utils';
+export { themes, themeNames, type TThemeName } from './themes';
+export {
+  colorsLib,
+  colorsStaticLightMode,
+  colorsStaticDarkMode,
+  shadowsStaticLightMode,
+  shadowsStaticDarkMode,
+} from './colors';
+
+export type { TTheme } from './interfaces';

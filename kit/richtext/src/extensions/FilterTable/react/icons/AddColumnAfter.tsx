3bde48ea1604d458cda9c9e9727d6d24e7c9f17e
kaarturov <kaarturov@sberbank.ru>
2025-02-22 05:34:30 +0300

    fix: minor

--- kit/richtext/src/extensions/FilterTable/react/icons/AddColumnAfter.tsx
+++ kit/richtext/src/extensions/FilterTable/react/icons/AddColumnAfter.tsx
@@ -4,21 +4,21 @@ const AddColumnAfter: FC = () => {
   return (
     <div style={{ padding: '4px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="icon icon-tabler icon-tabler-column-insert-left"
+        xmlns='http://www.w3.org/2000/svg'
+        className='icon icon-tabler icon-tabler-column-insert-left'
         width={24}
         height={24}
-        viewBox="0 0 24 24"
+        viewBox='0 0 24 24'
         strokeWidth={1.4}
-        stroke="currentColor"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        stroke='currentColor'
+        fill='none'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M14 4h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1z" />
-        <path d="M5 12l4 0" />
-        <path d="M7 10l0 4" />
+        <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+        <path d='M14 4h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1z' />
+        <path d='M5 12l4 0' />
+        <path d='M7 10l0 4' />
       </svg>
     </div>
   );
--- kit/richtext/src/utils/remapObject.ts
+++ kit/richtext/src/utils/remapObject.ts
@@ -1,16 +1,13 @@
-const remapObject = <T extends Record<string, unknown>>(
-    keysArray: (keyof T)[],
-    initialObject: T
-  ): Partial<T> => {
-    const remappedObject: Partial<T> = {};
-  
-    keysArray.forEach(key => {
-      if (initialObject.hasOwnProperty(key)) {
-        remappedObject[key] = initialObject[key];
-      }
-    });
-  
-    return remappedObject;
-  };
-  
-  export { remapObject };
+const remapObject = <T extends Record<string, unknown>>(keysArray: (keyof T)[], initialObject: T): Partial<T> => {
+  const remappedObject: Partial<T> = {};
+
+  keysArray.forEach((key) => {
+    if (initialObject.hasOwnProperty(key)) {
+      remappedObject[key] = initialObject[key];
+    }
+  });
+
+  return remappedObject;
+};
+
+export { remapObject };

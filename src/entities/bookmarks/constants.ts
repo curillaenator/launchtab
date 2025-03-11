77a839d4ac938f9910d2ea2933fff66a6e84a9bc
KirillArt <curillaenator@gmail.com>
2025-03-11 20:52:06 +0700

    fix: default bookmarks

--- src/entities/bookmarks/constants.ts
+++ src/entities/bookmarks/constants.ts
@@ -4,26 +4,34 @@ const DEFAULT_PAGES: BookmarkTabProps[] = [
   {
     name: 'Home',
     pages: [
-      { id: 'home_default_1', name: 'Facebook', link: 'facebook.com' },
-      { id: 'home_default_2', name: 'VK', link: 'vk.com' },
+      {
+        id: 'app_default_1',
+        name: 'About app',
+        link: 'https://launchtab-81b06.web.app/notes/jtG8WhhR5KHtpBxqtUs5',
+        iconURL: 'https://raw.githubusercontent.com/gilbarbara/logos/main/logos/firebase.svg',
+      },
+      {
+        id: 'app_default_2',
+        name: 'Tabs Guide',
+        link: 'https://launchtab-81b06.web.app/notes/m4fCGpakK6eDhqsDZfCB',
+        iconURL: 'https://raw.githubusercontent.com/gilbarbara/logos/main/logos/firebase.svg',
+      },
+      {
+        id: 'app_default_3',
+        name: 'Notes Guide',
+        link: 'https://launchtab-81b06.web.app/notes/Wa3oXR4V2vCuFBhfydJZ',
+        iconURL: 'https://raw.githubusercontent.com/gilbarbara/logos/main/logos/firebase.svg',
+      },
+      { id: 'home_default_1', name: 'Whatsapp', link: 'web.whatsapp.com' },
+      { id: 'home_default_2', name: 'Facebook', link: 'facebook.com' },
       { id: 'home_default_3', name: 'Instagram', link: 'instagram.com' },
-    ],
-  },
-  {
-    name: 'Office',
-    pages: [
-      { id: 'office_default_1', name: 'Gmail', link: 'gmail.com' },
-      { id: 'office_default_2', name: 'Outlook', link: 'outlook.live.com' },
-      { id: 'office_default_3', name: 'Zoom', link: 'zoom.us' },
-      { id: 'office_default_4', name: 'Skype', link: 'skype.com' },
-      { id: 'office_default_5', name: 'Google Meet', link: 'meet.google.com' },
+      { id: 'home_default_4', name: 'Gmail', link: 'gmail.com' },
     ],
   },
   {
     name: 'Video',
     pages: [
       { id: 'video_default_1', name: 'YouTube', link: 'youtube.com' },
-      { id: 'video_default_2', name: 'Vimeo', link: 'vimeo.com' },
       { id: 'video_default_3', name: 'Twitch', link: 'twitch.tv' },
       { id: 'video_default_4', name: 'Apple TV+', link: 'tv.apple.com' },
       { id: 'video_default_5', name: 'Netflix', link: 'netflix.com' },
@@ -32,11 +40,8 @@ const DEFAULT_PAGES: BookmarkTabProps[] = [
   {
     name: 'Music',
     pages: [
-      { id: 'music_default_1', name: 'Shazam', link: 'shazam.com' },
-      { id: 'music_default_2', name: 'Spotify', link: 'spotify.com' },
-      { id: 'music_default_3', name: 'Apple Music', link: 'music.apple.com' },
-      { id: 'music_default_4', name: 'Last.fm', link: 'last.fm' },
-      { id: 'music_default_5', name: 'Tidal', link: 'tidal.com' },
+      { id: 'music_default_1', name: 'Spotify', link: 'spotify.com' },
+      { id: 'music_default_2', name: 'Soundcloud', link: 'soundcloud.com' },
     ],
   },
 ];
